92f602dcb44d0b6e87e260a3e74610cdfccb5f61
rickD <debboutr@gmail.com>
2017-11-25 22:35:24 -0800

    work out data_group_detail

--- dashboard/models/data_group.py
+++ dashboard/models/data_group.py
@@ -1,14 +1,16 @@
+from datetime import date
+
 from django.db import models
 from django.core.urlresolvers import reverse
 
 from.data_source import DataSource
 
 class DataGroup(models.Model):
-	datasource = models.ForeignKey(DataSource)
+	datasource = models.ForeignKey('DataSource', on_delete=models.CASCADE)
 	name = models.CharField(max_length=50)
 	description = models.TextField(null=True, blank=True)
-	download_by = models.CharField(max_length=150)
-	download_date = models.DateTimeField(null=True)
+	download_by = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+	download_date = models.DateField(default=date.today)
 	scrape_script = models.CharField(max_length=150)
 
 	def __str__(self):
@@ -18,4 +20,4 @@ class DataGroup(models.Model):
 		return self.name
 
 	def get_absolute_url(self):
-		return reverse('data_group_edit', kwargs={'pk': self.pk})
+		return reverse('data_group_detail', kwargs={'pk': self.pk})
--- dashboard/views/data_source.py
+++ dashboard/views/data_source.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.forms import ModelForm
 from django.contrib.auth.decorators import login_required
 from dashboard.views import *
 from dashboard.models import DataSource, DataGroup
 from datetime import datetime
-
+from django.http import HttpResponseRedirect
 
 class DataSourceForm(ModelForm):
 	class Meta:
@@ -14,7 +14,10 @@ class DataSourceForm(ModelForm):
 class DataGroupForm(ModelForm):
 	class Meta:
 		model = DataGroup
-		fields = ['name', 'description', 'download_by', 'download_date', 'scrape_script']
+		fields = ['name', 'description', 'datasource', 'download_by', 'download_date', 'scrape_script']
+	def __init__(self, *args, **kwargs):
+		self.user = kwargs.pop('user', None)
+		super(DataGroupForm, self).__init__(*args, **kwargs)
 
 
 @login_required()
@@ -28,7 +31,15 @@ def data_source_list(request, template_name='data_source/datasource_list.html'):
 @login_required()
 def data_source_detail(request, pk, template_name='data_source/datasource_detail.html'):
 	datasource = get_object_or_404(DataSource, pk=pk, )
-	return render(request, template_name, {'object': datasource})
+	datagroup_list = DataGroup.objects.filter(datasource=pk)
+	request.session['datasource_title'] = datasource.title
+	request.session['datasource_pk'] = datasource.pk
+	#print(datasource.title,'|',datasource.pk)
+	context = {
+		'object': datasource,
+		'datagroup_list': datagroup_list,
+	}
+	return render(request, template_name, context)
 
 
 @login_required()
@@ -60,11 +71,48 @@ def data_source_delete(request, pk, template_name='data_source/datasource_confir
 	return render(request, template_name, {'object': datasource})
 
 @login_required()
-def data_group_create(request, pk, template_name='data_group/datagroup_form.html'):
-	form = DataGroupForm(request.POST or None)
-	datasource = get_object_or_404(DataSource, pk=pk)
-	print(datasource.id)
+def data_group_create(request, template_name='data_group/datagroup_form.html'):
+	datasource_title = request.session['datasource_title']
+	datasource_pk = request.session['datasource_pk']
+	new_group_key = DataGroup.objects.filter(datasource=datasource_pk).count() + 1
+	default_name = '{} {}'.format(datasource_title, new_group_key)
+	print(request.user)
+	initial_values = {'download_by': request.user, 'name': default_name, 'datasource':datasource_pk}
+	form = DataGroupForm(request.POST or None, initial=initial_values)
+	print(request.session.keys())
 	if form.is_valid():
 		form.save()
-		return redirect('data_source_list')
+		return HttpResponseRedirect(reverse('data_group_detail', kwargs={'pk': new_group_key}))
 	return render(request, template_name, {'form': form})
+
+# @login_required()
+# def data_group_list(request, template_name='data_group/datagroup_form.html'):
+# 	pass
+
+@login_required()
+def data_group_dets(request, pk, template_name='data_group/datagroup_detail.html'):
+
+	data_group = get_object_or_404(DataGroup, pk=pk)
+	print(request.path)
+	return render(request, template_name, {'datagroup': data_group})
+
+@login_required()
+def data_group_update(request, pk, template_name='data_group/datagroup_form.html'):
+	data_group = get_object_or_404(DataGroup, pk=pk)
+	form = DataGroupForm(request.POST or None, instance=data_group)
+	print(form)
+	if form.is_valid():
+		data_group.updated_at = datetime.now()
+		form.save()
+		return HttpResponseRedirect(reverse('data_group_detail', kwargs={'pk': pk}))
+	context = {'form': form,
+			   'object': data_group}
+	return render(request, template_name, context)
+
+@login_required()
+def data_group_delete(request, pk, template_name='data_source/datasource_confirm_delete.html'):
+	data_group = get_object_or_404(DataGroup, pk=pk)
+	if request.method == 'POST':
+		data_group.delete()
+		return redirect('data_source_list')
+	return render(request, template_name, {'object': data_group})
